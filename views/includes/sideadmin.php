928761bcfe8b5b656827e4db1d9d6dad06f168da
jersonortiz <jersonandresoc@ufps.edu.co>
2019-05-02 08:55:51 -0500

    dashboard usuarios

---
+++ views/includes/sideadmin.php
@@ -0,0 +1,24 @@
+    <!--sidebar-menu-->
+    <div id="sidebar">
+      <a href="#" class="visible-phone">
+        <i class="icon icon-home"></i> Dashboard</a>
+
+      <ul>
+
+        <li class="active">
+          <a href="index.html">
+            <i class="icon icon-home"></i>
+            <span>Listar cumplimiento</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="interface.html">
+            <i class="icon icon-pencil"></i>
+            <span>Configuracion</span>
+          </a>
+        </li>
+
+      </ul>
+    </div>
+    <!--sidebar-menu-->
